08d743802edaad9b536cbcbd4df687a9f768ef23
Ayoola <johnsonayoolaojo@gmail.com>
2026-07-07 14:40:58 +0100

    files

---
+++ my-app/src/App.jsx
@@ -0,0 +1,28 @@
+import Myself from "./components/Myself"
+
+
+
+
+
+function App() {
+
+
+  return (
+    <div style={{
+      backgroundColor:'yellow',
+      height: '90vh'
+      
+    }}>
+
+      <Myself/>
+      <Myself/>
+      <Myself/>
+      <Myself/>
+
+      {/* <Myprofile/> */}
+          
+    </div>
+  )
+}
+
+export default App
---
+++ my-app/src/components/Myself.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './myself.css'
+
+const Myself = () => {
+  return (
+    <div style={{
+        color: 'blue',
+    }}>
+       <h1>Hello World</h1>
+       <p>My name is Ayoola</p>
+           <p>I'm here to learn React!</p>
+    </div>
+  )
+}
+
+export default Myself
